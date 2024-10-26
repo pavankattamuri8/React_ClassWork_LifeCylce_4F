d6b2c0868147af51d9f832e9ca4caa724b88bea2
Pavan <pavankattamuri*@gmail.com>
2024-10-26 15:43:05 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/autoFocus/"
+  base:"/React_ClassWork_LifeCylce_4F/"
 })
